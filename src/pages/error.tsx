359127c32a32ba5b41d5b02cedbf07486116b2da
dovid-moshe-crow <dovid-moshe-crow@users.noreply.github.com>
2023-02-01 18:06:03 +0200

    Update error.tsx

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router";
 
 function ErrorPage() {
-    const router = useRouter();
-    const message = router.query.message;
+  const router = useRouter();
+  const message = router.query.message;
   return (
     <div className="h-screen w-screen bg-red-500 text-center text-white">
       <h1 className="mb-4 text-5xl font-bold">תקלה</h1>
       <p className="mb-8 text-lg">{message}</p>
       <a
+        target="_top"
         href="/"
         className="rounded-lg bg-white px-4 py-2 text-red-500 hover:bg-red-500 hover:text-white"
       >
